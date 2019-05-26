e95beabeb718f8c0fce4c0b7446d004518ac41e1
cary205 <cary205@hotmail.com>
2019-05-26 16:12:59 +0800

    fix basic auth cors

--- src/main/java/org/springframework/samples/petclinic/security/BasicAuthenticationAdapter.java
+++ src/main/java/org/springframework/samples/petclinic/security/BasicAuthenticationAdapter.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,10 +24,12 @@ public class BasicAuthenticationAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/securityNone").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .httpBasic().and()
+            .cors().and()
             .csrf().disable();
     }
 
